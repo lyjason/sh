c06bf49421c731b2c52b402b6e58033c492b8700
root <root@localhost.(none)>
2016-09-24 16:48:51 +0800

    yum update

--- yum_media_off.sh
+++ yum_media_off.sh
@@ -2,5 +2,6 @@
 
 umount /dev/sr0 
 sed -i -e 's|enabled=1|enabled=0|' /etc/yum.repos.d/CentOS-Media.repo
+/bin/mv /etc/yum.repos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo  
 echo "cdrom yum was released!"
 
--- yum_media_on.sh
+++ yum_media_on.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mount /dev/sr0 /mnt/cdrom  > /dev/null 2>&1
 sed -i -e 's|enabled=0|enabled=1|' /etc/yum.repos.d/CentOS-Media.repo
+sed -i -e 's|baseurl=file:///media/CentOS/|baseurl=file:///mnt/cdrom/|' /etc/yum.repos.d/CentOS-Media.repo
+/bin/mv /etc/yum.repos.d/CentOS-Base.repo  /etc/yum.repos.d/CentOS-Base.repo.bak
 echo "cdrom yum is loading!"
