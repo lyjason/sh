e9f87794c68fe2fb76ef6e2bb3d833415a613e52
root <root@localhost.(none)>
2016-10-12 16:33:55 +0800

    update

--- python.sh
+++ python.sh
@@ -3,15 +3,17 @@
 # Author:Jason lya1b2333@hotmail.com
 #检测python版本并提供修改
 
-read -p "please input the present version of Python(2|3): " ver
+read -p "which version would you want (2|3): " ver
 case $ver in
 	"2")
 		rm -rf /usr/bin/python
 		ln -s /usr/local/python2.7.12/bin/python2.7 /usr/bin/python	
+		echo 'python_v2 on line'
 		;;
 	"3")
 		path=$(whereis python3|cut -d ' ' -f 2 )
 		rm -rf  /usr/bin/python
 		ln -s $path  /usr/bin/python	
+		echo 'python_v3 on line'
 		;;
 esac
