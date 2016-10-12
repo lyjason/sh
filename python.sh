d103bf7adc0fd19b95c358701d2ceeb15d84bb43
root <root@localhost.(none)>
2016-10-12 13:51:50 +0800

    new

---
+++ python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#-*-coding:utf-8-*-
+# Author:Jason lya1b2333@hotmail.com
+#检测python版本并提供修改
+
+read -p "please input the present version of Python(2|3): " ver
+case $ver in
+	"2")
+		/bin/bash python2-3.sh
+		;;
+	"3")
+		/bin/bash python3-2.sh
+		;;
+esac
