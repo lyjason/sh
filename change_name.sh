cebf45991c22ddfced19dbe73e4b4e1baa7262d3
root <root@CESE-Server.(none)>
2016-10-12 11:18:05 +0800

    new

--- change_name.sh
+++ change_name.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i -e 's|#!/usr/bin/python|#!/usr/bin/python2.6|' /root/py/*
+sed -i -e 's|#!/usr/bin/python*|#!/usr/bin/python2.6|' ~/py/*
 echo 'successfull!'
---
+++ python3-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#-*-coding:utf-8-*-
+# Author:Jason lya1b2333@hotmail.com
+mv /usr/bin/python /usr/bin/python.bak.3
+mv /usr/bin/python.bak.2 /usr/bin/python
+echo "NOW Python is v2"
